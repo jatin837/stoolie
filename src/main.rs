79e81b6110fee2dcc2dbdd753c7b184fa626590f
Dj4t9n <pandeyjatin47@gmail.com>
2021-06-09 17:44:52 +0530

    error, str and string are not same

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,13 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+
+    let query = &args[1];
+    let filename = &args[2];
+    let possible_query = String::new("list")
+
+    match query {
+        possible_query => println!("------------------listing issues in {}--------", filename), 
+        _ => println!("TOBE IMPLEMENTED IN FUTURE PERHAPS NOT IF IT DOES NOT MAKE SENSE"),
+    };
 }
