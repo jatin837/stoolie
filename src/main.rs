159325b6b952379e93d81aff106db02c14d047bd
Dj4t9n <pandeyjatin47@gmail.com>
2021-06-10 16:13:33 +0530

    now remove date regex with issue template

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,14 @@ use std::path::Path;
 use path_abs::PathAbs;
 use std::fs::File;
 use std::io::Read;
+use regex::Regex;
 
 fn main() {
+    let re = Regex::new(r"(\d{4})-(\d{2})-(\d{2})").unwrap();
+    let text = "2012-03-14, 2013-01-01 and 2014-07-05";
+    for cap in re.captures_iter(text) {
+        println!("Month: {} Day: {} Year: {}", &cap[2], &cap[3], &cap[1]);
+    }
     let args: Vec<String> = env::args().collect();
 
     let filename: &String = &args[1];
