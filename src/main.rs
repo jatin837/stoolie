8d6cabadab681b47e034d3f40d33d5da12308b4a
Dj4t9n <pandeyjatin47@gmail.com>
2021-06-10 14:13:53 +0530

    getting cannonicallized absolute file path change it

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 use std::env;
+use std::path::Path;
+use path_abs::PathAbs;
 //use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
-    let possible_query = String::new("list")
+    let filename: &String = &args[1];
+    let filepath: &Path = Path::new(filename);
+    let absFilePath = PathAbs::new(filepath);
+    
+    // get absolute path of filename
+    println!("{:?}", filepath);
+    println!("{:?}", filename);
+    println!("{:?}", absFilePath);
 
-    match query {
-        possible_query => println!("------------------listing issues in {}--------", filename), 
-        _ => println!("TOBE IMPLEMENTED IN FUTURE PERHAPS NOT IF IT DOES NOT MAKE SENSE"),
-    };
+   
 }
