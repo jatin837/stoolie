72daed88ea6c7b5f1dd4292f436300ad362b9ec9
Dj4t9n <pandeyjatin47@gmail.com>
2021-06-10 14:36:50 +0530

    need to work inside match-Ok block

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,19 @@
 use std::env;
 use std::path::Path;
 use path_abs::PathAbs;
-//use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let filename: &String = &args[1];
-    let filepath: &Path = Path::new(filename);
-    let abs_file_path = PathAbs::new(filepath);
+    
+    let abs_file_path = PathAbs::new(filename);
     match abs_file_path {
-        Ok(v) => println!("absolute path is {:?}", v),
+        Ok(v) => {
+            let filepath:&Path = Path::new(&v);
+            println!("{:?}", filepath);
+        },
         Err(e) => println!("error {:?}", e)
     }
-    
-    // get absolute path of filename
-    println!("{:?}", filepath);
     println!("{:?}", filename);
-
-   
 }
