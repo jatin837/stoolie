4780bee5e4ab40e28b5fe175a2f48e66202b6247
Dj4t9n <pandeyjatin47@gmail.com>
2021-06-10 15:04:44 +0530

    Compilation error

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::path::Path;
 use path_abs::PathAbs;
+use std::fs::File;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,6 +15,10 @@ fn main() {
             let is_exist: bool = filepath.exists();
             if is_exist {
                 println!("{:?}", filepath);
+                let mut file = File::open(filepath)?;
+                let mut contents = String::new();
+                file.read_to_string(&mut contents)?;
+
             } else {
                 println!("No such file exists right now")
             }
