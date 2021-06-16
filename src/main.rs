35518b36c12c8ed138057fdfda2a194edebab65c
Dj4t9n <pandeyjatin47@gmail.com>
2021-06-16 23:19:28 +0530

    STATUS: failed to parse regexp, unrecognized escape sequence TODO: inspect wny \ is labled as "unrecognized"

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,7 @@ use std::io::Read;
 use regex::Regex;
 
 fn main() {
-    let re = Regex::new(r"(\d{4})-(\d{2})-(\d{2})").unwrap();
-    let text = "2012-03-14, 2013-01-01 and 2014-07-05";
-    for cap in re.captures_iter(text) {
-        println!("Month: {} Day: {} Year: {}", &cap[2], &cap[3], &cap[1]);
-    }
+    let re = Regex::new(r"\/\/ *-*TODO-*\n\/\/([a-zA-Z1-9 ]*)").unwrap();
     let args: Vec<String> = env::args().collect();
 
     let filename: &String = &args[1];
@@ -24,7 +20,11 @@ fn main() {
                 println!("{:?}", filepath);
                 let mut file = File::open(filepath).expect("Unable To open the file");
                 let mut contents = String::new();
+
                 file.read_to_string(&mut contents).expect("can not read file contents");
+                for cap in re.captures_iter(&contents) {
+                    println!("Issues are : {}", &cap[0]);
+                }
                 println!("contents of file are :::  {:?}", contents);
 
             } else {
