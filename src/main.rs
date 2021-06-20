1b30741183b05658e4cad694c58561c246892635
Dj4t9n <pandeyjatin47@gmail.com>
2021-06-20 11:08:26 +0530

    stored into a vector issues

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use regex::Regex;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let mut issues_list:Vec<String> = Vec::new();
+
     let re = Regex::new(r" *-*TODO-*\n([a-zA-Z1-9 ]*)").unwrap();
 
     let filename: &String = &args[1];
@@ -26,14 +28,9 @@ fn main() {
     file.read_to_string(&mut contents).expect("can not read file contents");
     for cap in re.captures_iter(&contents) {
         println!("Issues are : {}", &cap[0]);
+        issues_list.push(String::from(&cap[0]))
     }
-    println!("--------------------");
-    println!("--------------------");
-    println!("--------------------");
-    println!("--------------------");
     print!("contents of file are :::  {:?}", contents);
-    println!("--------------------");
-    println!("--------------------");
-    println!("--------------------");
+    println!("DONE STORED IN ISSUES_LIST");
 
 }
