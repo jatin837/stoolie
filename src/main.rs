6ccd700378178dbec1f1ac5e3881a692f60cf5a9
dj4t9n <pandeyjatin47@gmail.com>
2021-06-28 14:01:40 +0530

    yaml parser working

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 extern crate yaml_rust;
 use yaml_rust::{YamlLoader, YamlEmitter};
+use std::env;
+use std::fs;
 
 fn main() {
-    let s =
-"
-foo:
-    - list1
-    - list2
-bar:
-    - 1
-    - 2.0
-";
-    let docs = YamlLoader::load_from_str(s).unwrap();
+    let filename = "test.yaml";
+    
+    let contents = fs::read_to_string(filename).expect("something went wrong");
+
+    println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    let docs = YamlLoader::load_from_str(&contents).unwrap();
 
     // Multi document support, doc is a yaml::Yaml
     let doc = &docs[0];
@@ -19,20 +21,11 @@ bar:
     // Debug support
     println!("{:?}", doc);
 
-    // Index access for map & array
-    assert_eq!(doc["foo"][0].as_str().unwrap(), "list1");
-    assert_eq!(doc["bar"][1].as_f64().unwrap(), 2.0);
-
-    // Chained key/array access is checked and won't panic,
-    // return BadValue if they are not exist.
-    assert!(doc["INVALID_KEY"][100].is_badvalue());
-
-    // Dump the YAML object
     let mut out_str = String::new();
     {
         let mut emitter = YamlEmitter::new(&mut out_str);
         emitter.dump(doc).unwrap(); // dump the YAML object to a String
     }
     println!("{}", out_str);
+    println!("With text:\n{}", contents);
 }
-
