051cdfbf268d114f4f26c85ddb41a14b75f647e2
dj4t9n <pandeyjatin47@gmail.com>
2021-07-29 11:37:41 +0530

    issue resolved regarding config

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,21 @@
-use std::env;
-use std::str;
-use std::path::Path;
 use path_abs::PathAbs;
-use std::fs::{
-    File, 
-    read_to_string,
-};
-use std::io::Read;
 use regex::Regex;
+use std::env;
+use std::fs::{read_to_string, File};
+use std::io::Read;
+use std::path::Path;
+use std::str;
 //use std::collections::HashMap;
 //use hex_literal::hex;
-use sha2::{
-    Sha256,
-    Digest,
-};
-use yaml_rust::{YamlLoader, Yaml};
+use sha2::{Digest, Sha256};
+use yaml_rust::{Yaml, YamlLoader};
 
 // (file path) => |my function name| => (string fromm that file)
 
 fn string_from_file(fpath: &String) -> String {
     let rel_path: &Path = Path::new(fpath);
     let abs_file_path = PathAbs::new(rel_path).unwrap();
-    let filepath:&Path = Path::new(&abs_file_path);
+    let filepath: &Path = Path::new(&abs_file_path);
     if !filepath.exists() {
         panic!("File does not exist")
     }
@@ -29,7 +23,8 @@ fn string_from_file(fpath: &String) -> String {
     let mut file = File::open(filepath).expect("Unable To open the file");
     let mut contents = String::new();
 
-    file.read_to_string(&mut contents).expect("can not read file contents");
+    file.read_to_string(&mut contents)
+        .expect("can not read file contents");
     contents
 }
 
@@ -46,7 +41,7 @@ struct Issue {
 }
 
 impl Issue {
-    fn post_issue(idle: Vec<Issue>){
+    fn post_issue(idle: Vec<Issue>) {
         //post idle issues to github
     }
 }
@@ -61,8 +56,8 @@ impl Issues {
         iss
     }
     fn load_issues() {
-       //load unposted issues from .ISSUES file 
-       todo!()
+        //load unposted issues from .ISSUES file
+        todo!()
     }
 }
 
@@ -75,8 +70,8 @@ fn hash(issue: String) -> String {
     let mut hasher = Sha256::new();
     hasher.update(issue.as_bytes());
     let result = hasher.finalize();
-    let mut ret_res:Vec<char> = Vec::new();
-    for c in result{
+    let mut ret_res: Vec<char> = Vec::new();
+    for c in result {
         ret_res.push(c as char)
     }
     let res: String = ret_res.into_iter().collect();
@@ -89,17 +84,30 @@ struct User {
     email: String,
 }
 
-fn load_config<'a>() ->  Vec<&'a str> {
-
-    let test_config: &String = &String::from("test.yaml");
-    let configs: Vec<Yaml> = parse_yaml(test_config);
-    let name: &str = configs[0]["user"][0].as_str().expect("something wrong");
-    let email: &str = configs[0]["user"][1].as_str().expect("something wrong");
-    let acces_token: &str = configs[0]["user"][2].as_str().expect("something wrong");
-    println!("name -> {:?}, email -> {:?}, access_token -> {:?}", name, email, acces_token);
+fn load_config() -> Vec<String> {
+    let test_config_path: &String = &String::from("test.yaml");
+    let configs: Vec<Yaml> = parse_yaml(test_config_path);
+    let name: String = configs[0]["user"][0]
+        .as_str()
+        .expect("something wrong")
+        .clone()
+        .to_string();
+    let email: String = configs[0]["user"][1]
+        .as_str()
+        .expect("something wrong")
+        .clone()
+        .to_string();
+    let acces_token: String = configs[0]["user"][2]
+        .as_str()
+        .expect("something wrong")
+        .clone()
+        .to_string();
+    println!(
+        "name -> {:?}, email -> {:?}, access_token -> {:?}",
+        name, email, acces_token
+    );
     vec![name, email, acces_token]
 }
-
 
 fn read_args() -> Vec<String> {
     let args: Vec<String> = env::args().collect();
@@ -108,26 +116,28 @@ fn read_args() -> Vec<String> {
 
 fn main() {
     let args = read_args();
-    let mut issues:Vec<Issue> = Issues::new();
 
-    let re = Regex::new(r" *-*TODO-*([a-zA-Z1-9 ]*)").expect("you fuckin meet head");
-
-    let contents = string_from_file(&args[1]);
-
-    for cap in re.captures_iter(&contents) {
-        issues.push(Issue{
-            heading: String::from(&cap[0]),
-            status: Status::Idle,
-            digest: hash(String::from(&cap[0]))
-        })
+    if args.len() == 1 {
+        println!("NEED AN ARGUMENT");
+        std::process::exit(1);
     }
 
-    load_config();
-
-    
-    for issue in issues{
+    let mut issues: Vec<Issue> = Issues::new();
+    let re = Regex::new(r" *-*TODO-*([a-zA-Z1-9 ]*)").expect("you fuckin meet head");
+    let contents = string_from_file(&args[1]);
+    for cap in re.captures_iter(&contents) {
+        issues.push(Issue {
+            heading: String::from(&cap[0]),
+            status: Status::Idle,
+            digest: hash(String::from(&cap[0])),
+        })
+    }
+    let config_vals: Vec<String> = load_config();
+    for config_val in config_vals.iter() {
+        println!("{:?} ", config_val);
+    }
+    for issue in issues {
         println!("--{}--{}--", issue.heading, issue.digest)
     }
     println!("DONE STORED IN ISSUES_LIST");
-
 }
